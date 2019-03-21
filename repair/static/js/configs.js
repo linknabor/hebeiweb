68306eef041cda3a951f0f409919d18d4551c07b
天使之翼唐 <476749176@qq.com>
2019-03-21 16:16:26 +0800

    兴社区 报修板块 更换框架

---
+++ repair/static/js/configs.js
@@ -0,0 +1,5 @@
+window.configs = {
+		url1:/127|test/.test(location.origin)?'https://test.e-shequ.com/weixin/orderpay.html':
+		/uat/.test(location.origin)?'https://uat.e-shequ.com/weixin/orderpay.html':
+		'https://www.e-shequ.com/shijiazhuang/weixin/orderpay.html',		
+}
