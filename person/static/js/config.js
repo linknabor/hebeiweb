ff05e6e71314ee85ae6af9f8363a8c7891dda828
天使之翼唐 <476749176@qq.com>
2019-03-26 14:47:16 +0800

    兴社区个人中心

--- person/static/js/config.js
+++ person/static/js/config.js
@@ -1,33 +1,26 @@
+var pay = /127|test/.test(location.origin)?'https://test.e-shequ.com/pay/':
+		  /uat/.test(location.origin)?'https://uat.e-shequ.com/weixin/':
+		  'https://www.e-shequ.com/pay/';
+var common =/127|test/.test(location.origin)?'https://test.e-shequ.com/weixin/':
+			/test/.test(location.origin)?'https://uat.e-shequ.com/xsq/weixin/':
+			'https://www.e-shequ.com/shijiazhuang/weixin/';
 let config = {
+	//请求头公共部分
+	baseUrl:/127|test/.test(location.origin)?'https://test.e-shequ.com/wechat/hexie/wechat/':
+  			/uat/.test(location.origin)?'https://uat.e-shequ.com/xsq/wechat/hexie/wechat/':
+  			'https://www.e-shequ.com/shijiazhuang/wechat/hexie/wechat/',
 	//个人中心
-		//首页请求
-		person_index:{
-			//商品订单
-			url1:/127|test/.test(location.origin)?'https://test.e-shequ.com/pay/'+'shijiazhuang'+'orderpay.html':
-			/uat/.test(location.origin)?'https://uat.e-shequ.com/weixin/'+ 'shijiazhuang'+'orderpay.html':
-			'https://www.e-shequ.com/pay/'+'shijiazhuang'+'orderpay.html',
-
-			//报修单
-			url2:/127|test/.test(location.origin)?'https://test.e-shequ.com/weixin/repair/index.html':
-			/test/.test(location.origin)?'https://uat.e-shequ.com/xsq/weixin/repair/index.html':
-			'https://www.e-shequ.com/shijiazhuang/weixin/repair/index.html',
-
-			//绑定房屋 我是业主
-			url3:/127|test/.test(location.origin)?'https://test.e-shequ.com/weixin/wuye/index.html?#/myHouse':
-			/uat/.test(location.origin)?'https://uat.e-shequ.com/xsq/weixin/wuye/index.html?#/myHouse':
-			'https://www.e-shequ.com/shijiazhuang/weixin/wuye/index.html?#/myHouse',
+		person_index:{			
+			url1: pay+'shijiazhuang'+'orderpay.html',//商品订单 团购订单 服务订单			
+			url2: common+'repair/index.html',//报修单			
+			url3: common+'wuye/index.html?#/myHouse',//绑定房屋 我是业主
 		},
-		person_footer:{
-			//底部导航 社区
-			url1:/127|test/.test(location.origin)?'https://test.e-shequ.com/weixin/home/index.html':
-			/uat/.test(location.origin)?'https://uat.e-shequ.com/xsq/weixin/home/index.html':
-			'https://www.e-shequ.com/shijiazhuang/weixin/home/index.html'
+		person_footer:{			
+			url1: common+'home/index.html',//底部导航 社区
 		},
 		person_register:{
 			// 注册
-			url1:/127|test/.test(location.origin)?'https://test.e-shequ.com/weixin/person/index.html':
-			/uat/.test(location.origin)?'https://uat.e-shequ.com/xsq/weixin/person/index.html':
-			'https://www.e-shequ.com/shijiazhuang/weixin/person/index.html'
+			url1: common+'person/index.html',
 		}
 	
 		
